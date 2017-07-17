cb9ea30edc02569585dc0fa4b65956134ba9182e
Lori Oliver <loridoesitbest@gmail.com>
2017-07-17 13:31:33 -0400

    Optimized Code

--- Gen9.java
+++ Gen9.java
@@ -2,8 +2,13 @@ import java.util.Random;
 import java.io.PrintWriter;
 import java.io.IOException;
 
+/*
+* Generates a nine digit number, consisting of numbers 1 - 9. 
+* Each number can only appear once
+*/
 public class Gen9{
 	public static void main(String[] args){
+		// Used to denote whether a number has already been chosen.
 		int[] x = {0, 0, 0,
 				   0, 0, 0,
 				   0, 0, 0};
@@ -11,13 +16,11 @@ public class Gen9{
 		String state = "";
 		Random randy = new Random();
 		boolean leave = false;
+		int rand = 0;
 		while(leave == false)
 		{
-			int rand = randy.nextInt(9);
-			if(x[rand] > 0){
-				rand = randy.nextInt(9);
-			}
-			else{
+			rand = randy.nextInt(9);
+			if(x[rand] == 0){
 				x[rand]++;
 				state += rand;
 				//System.out.println("CurState: " + state);
